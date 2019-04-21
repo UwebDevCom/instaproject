37cb89737aba1ea312c05fc32a63075bd0d17a36
elazar durlachr <elazar91@gmail.com>
2019-04-21 11:52:19 +0300

    start search  component and css

--- src/Components/appRouter/search/Search.js
+++ src/Components/appRouter/search/Search.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
-import {useSetat, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './search.css';
 
 const Search = () =>{
-    const [search, setSearch] = useSetat(search);
-    
+    const [search, setSearch] = useState("search");
+
         return(
             <div className="appRouterSearchBox">
                 <input type="text" placeholder={search}></input>
+                <span className="appRouterMagnifyingGlass"></span>
             </div>   
         )
     }
