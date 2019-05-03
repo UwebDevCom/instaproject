d2df93749f17d67f3a67fb7a58f9a93fa64e210a
Tomer Matmon <tomer.matmon@gmail.com>
2019-05-03 13:21:44 +0300

    made changes to user, post and comment models

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -7,10 +7,7 @@ export default class UsersService {
         return users;
     }
     async savePost(userId,saved) {
-<<<<<<< HEAD
-=======
         console.log(saved);
->>>>>>> a5dcf253450650ee3766e130e0ad03cc6bfb302c
         await fetch(SERVER_URL +'users/'+ userId, {
             method: 'PUT',
             headers: {
