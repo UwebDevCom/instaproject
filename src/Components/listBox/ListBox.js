8d4efcbb913130aaf3db266bbf122fe8cd354f4f
Tomer Matmon <tomer.matmon@gmail.com>
2019-05-04 22:41:18 +0300

    fixes to userpage

---
+++ src/Components/listBox/ListBox.js
@@ -0,0 +1,15 @@
+import React, { useContext, useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+export default function ListBox() {
+    const Wrapper = styled.div`
+        width: 200px;
+        height: 200px;
+        background-color: red;
+    `
+    return (
+        <Wrapper>
+          <h1>hello</h1>  
+        </Wrapper>
+    )
+}
