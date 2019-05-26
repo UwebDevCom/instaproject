a6e4757dcf164f9ca5efd9c900b6c782cf055ba0
Tomer Matmon <tomer.matmon@gmail.com>
2019-05-27 01:08:06 +0300

    working on user page

--- src/Components/UserPage/Channel.js
+++ src/Components/UserPage/Channel.js
@@ -1,18 +1,16 @@
 import React from 'react';
-// import './channel.scss';
+import './channel.scss';
 import { BrowserRouter as Router, Link} from 'react-router-dom';
 
 export default function Channel() {
     return ( 
-        <Router>
-            <div className='channel-container'>
-                <div className='channel-icon' />    
-                <h1>Upload a Video</h1>
-                <p>Videos must be vertical and between 15 seconds and 10 minutes long.</p>
-                <button>
-                    <Link to=''>Upload</Link>
-                </button>
-            </div>
-        </Router>
+        <div className='channel-container'>
+            <div className='channel-icon' />    
+            <h1>Upload a Video</h1>
+            <p>Videos must be vertical and between 15 seconds and 10 minutes long.</p>
+            <button>
+                <Link to=''>Upload</Link>
+            </button>
+        </div>
     )    
 }
